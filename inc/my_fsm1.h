589bb3a40ecd57faabf66f5c5a97bb1f28b705df
X13TP\edward <edward.jw.yu@outlook.com>
2023-07-08 10:47:32 +0800

    首次提交，发布v1.0.0版本

---
+++ inc/my_fsm1.h
@@ -0,0 +1,30 @@
+/* finite state machine */
+#ifndef __MY_FSM1_H__
+#define __MY_FSM1_H__
+
+typedef struct
+{
+    int event;             // 事件
+    int cur_state;         // 当前状态
+    int nxt_state;         // 下一个状态
+    void (*handler)(void); // 事件处理句柄
+} my_fsm1_table_t, *my_fsm1_table_pt;
+
+typedef struct
+{
+    int lst_state; // 上一次状态
+    int cur_state; // 当前状态
+    int tb_size;
+    my_fsm1_table_pt table;
+} my_fsm1_t, *my_fsm1_pt;
+
+/* 初始化有限状态机 */
+void my_fsm1_init(my_fsm1_pt p_fsm1, my_fsm1_table_pt p_fsm1_tb, int tb_size);
+
+/* 切换状态 */
+void my_fsm1_transform(my_fsm1_pt p_fsm1, int state);
+
+/* 状态机处理事件 */
+void my_fsm1_handle_event(my_fsm1_pt p_fsm1, int event);
+
+#endif // __MY_FSM1_H__
---
+++ src/my_fsm1.c
@@ -0,0 +1,44 @@
+#include "my_fsm1.h"
+#include <rtthread.h>
+
+/* 初始化有限状态机 */
+void my_fsm1_init(my_fsm1_pt p_fsm1, my_fsm1_table_pt p_fsm1_tb, int tb_size)
+{
+    if ((!p_fsm1) || (!p_fsm1_tb)) return;
+
+    p_fsm1->table = p_fsm1_tb;
+    p_fsm1->tb_size = tb_size;
+}
+
+/* 切换状态 */
+void my_fsm1_transform(my_fsm1_pt p_fsm1, int state)
+{
+    if (!p_fsm1) return;
+    
+    p_fsm1->lst_state = p_fsm1->cur_state; // 保存上一次状态
+    p_fsm1->cur_state = state;
+
+    rt_kprintf("[fsm1] lst_state: %d, cur_state: %d\r\n", p_fsm1->lst_state, p_fsm1->cur_state);
+}
+
+/* 状态机处理事件 */
+void my_fsm1_handle_event(my_fsm1_pt p_fsm1, int event)
+{
+    if (!p_fsm1) return;
+
+    /* 获取事件处理函数 */
+    for (int i = 0; i < p_fsm1->tb_size; i++)
+    {
+        // 当且仅当当前状态下来个指定的事件，我才执行它
+        if ((event == p_fsm1->table[i].event) && (p_fsm1->cur_state == p_fsm1->table[i].cur_state))
+        {
+            // 进入下一个状态
+            my_fsm1_transform(p_fsm1, p_fsm1->table[i].nxt_state);
+            if (p_fsm1->table[i].handler)
+            { // 执行事件处理函数
+                p_fsm1->table[i].handler();
+            }
+            break;
+        }
+    }
+}
